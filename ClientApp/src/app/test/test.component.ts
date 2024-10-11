452d71d3dd7649f4cf1267c97ec426d5dac72464
Enrique Diaz <diazenrive@gmail.com>
2024-10-11 18:09:35 -0400

    .
    .

--- ClientApp/src/app/test/test.component.ts
+++ ClientApp/src/app/test/test.component.ts
@@ -8,17 +8,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./test.component.css']
 })
 export class testComponent {
+
   constructor(private http: HttpClient,private router: Router, @Inject('BASE_URL') private baseUrl: string) 
   { 
-    this.prueba_test();
+    this.prueba_get();
   }
 
-  prueba_test()
+  prueba_get()
   {
     this.http.get<Equipo[]>(this.baseUrl + 'equipo/seeEquipos').subscribe(
       result=>{
         console.log(result);
       }, error=>{console.log("error");})
+  }
+
+  prueba_post(Id_equipo: string, Codigo_Equipo: string, Nombre_equipo: string, Estado_Equipo: string, Detalle_equipo: string) {
+    // Convertir el Id_equipo a número
+    const idEquipoNumber = Number(Id_equipo);
+
+    // Crear el objeto equipo basado en los parámetros recibidos
+    const nuevoEquipo = {
+      Id_equipo: idEquipoNumber,
+      Codigo_Equipo,
+      Nombre_equipo,
+      Estado_Equipo,
+      Detalle_equipo
+    };
+
+    // Hacer la solicitud POST
+    this.http.post<string>(this.baseUrl + 'equipo/addEquipos', nuevoEquipo)
+      .subscribe(
+        result => {
+          console.log('Success:', result);
+          alert('Equipo registrado con éxito');
+        },
+        error => {
+          console.error('Error:', error);
+          alert('Error al registrar el equipo');
+        }
+      );
   }
 }
 
